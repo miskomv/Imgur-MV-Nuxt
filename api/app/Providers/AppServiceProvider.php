5e6b2c4866dfaa25eb5d3b7b239749b74482318c
miSko <36664000+miskomv@users.noreply.github.com>
2019-06-23 14:36:06 +0200

    Laravel Serverless changes

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -6,23 +6,25 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		if (!is_dir(config('view.compiled'))) {
+			mkdir(config('view.compiled'), 0755, true);
+		}
+	}
 }
